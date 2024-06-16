76d3c77ad0db0a4db7b5189e3d57f7a47e2c0d42
IgromanA <artemii200588@gmail.com>
2024-06-16 23:55:40 +0700

    Lab work 5

--- Labwork 5.py
+++ Labwork 5.py
@@ -17,12 +17,12 @@ def Y(x, a):
 
 
 x1, x2, a1, a2 = map(int, input('Enter the limits for x and a: ').split())
-flag = int(input('Enter String of steps to calculate: '))
-key = int(input('Choose an equation:\n' + 
+flag = int(input('\nEnter String of steps to calculate: '))
+key = int(input('\nChoose an equation:\n' + 
                 '1 - Equation G\n' + 
                 '2 - Equation F\n' + 
                 '3 - Equation Y\n'))
-pat = float(input('Enter String pattern: '))
+pat = float(input('\nEnter String pattern: '))
 cnt = 0
 ans, pairs, Nn = [], [], []
 Mark = False
@@ -33,30 +33,30 @@ for a in range(a1, a2 + 1):
             if key == 1:
                 fun = 'G'
                 if not(G(x, a)):
-                    pairs.extend([0, x])
+                    pairs.extend([0, x, a])
                     ans.append(pairs)
                     pairs = []
                     cnt += 1
                 else:
-                    pairs.extend([round(G(x, a), 5), x])
+                    pairs.extend([round(G(x, a), 5), x, a])
                     ans.append(pairs)
                     pairs = []
                     cnt += 1
             elif key == 2:
                 fun = 'F'
                 if not(F(x, a)):
-                    pairs.extend([0, x])
+                    pairs.extend([0, x, a])
                     ans.append(pairs)
                     pairs = []
                     cnt += 1
                 else:
-                    pairs.extend([round(F(x, a), 5), x])
+                    pairs.extend([round(F(x, a), 5), x, a])
                     ans.append(pairs)
                     pairs = []
                     cnt += 1
             elif key == 3:
                 fun = 'Y'
-                pairs.extend([round(Y(x, a), 5), x])
+                pairs.extend([round(Y(x, a), 5), x, a])
                 ans.append(pairs)
                 pairs = []
                 cnt += 1
@@ -66,16 +66,15 @@ for a in range(a1, a2 + 1):
             break
 
 if key in [1, 2, 3]:
-    print(f'x | {fun}\n{max(ans)[1]} | {float(max(ans)[0])}\n{min(ans)[1]} | {float(min(ans)[0])}')
+    print(f'\n     x | a | {fun}\nMax: {max(ans)[1]} | {max(ans)[2]} | {float(max(ans)[0])}\nMin: {min(ans)[1]} | {min(ans)[2]} | {float(min(ans)[0])}\n')
     for i in range(len(ans)):
         print(ans[i][0], end=" ")
         if ans[i][0]== pat:
             Mark = True
             Nn.append(i+1)
-            
     if Mark:
-        print(f'\nString find {Nn}')
+        print(f'\n\nString matching the pattern found\n{int(Nn[0])} sequence element\nTotal matching elements: {len(Nn)}')
     else:
-        print(f'\nString not find')
+        print(f'\n\nString matching the pattern not found\nTotal matching elements: 0')
 else:
     print('Incorrect value')
---
+++ Labwork 5_1.py
@@ -0,0 +1,12 @@
+num = input('Enter a number: ')
+
+cnt = 0
+even = '02468'
+
+if float(num).is_integer():    
+    for i in num:
+        if i in even:
+            cnt += 1
+else: None
+
+print(f'Count of even digits: {cnt}')
